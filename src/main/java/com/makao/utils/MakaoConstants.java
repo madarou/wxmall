2499def32a3cb8ef0043c96af747594ebfb1c0e4
makao <2016333261@qq.com>
2016-10-17 13:54:51 +0800

    change img js css to cdn

--- src/main/java/com/makao/utils/MakaoConstants.java
+++ src/main/java/com/makao/utils/MakaoConstants.java
@@ -9,6 +9,7 @@ public class MakaoConstants {
 	public static final int PAGE_SIZE = 10;//商品列表，券列表，用户列表等分页每页的记录数
 	public static final long TOKEN_EXPIRE = 5;//系统自定义token在缓存中保留的时间(暂设为小时)
 	public static final String SERVER_DOMAIN = "http://m1.shequvip.com";//最后部署上线的服务器的域名，用于设置微信回调页面
+	public static final String CDN_DOMAIN="http://sx-1252349799.cosgz.myqcloud.com";//
 	
 	public static final long ORDER_EXPIRE = 10;//下单等待支付的时间限制，单位为分(10)
 	
