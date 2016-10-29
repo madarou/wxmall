e5ff2f9bbd4bfd34f5608fe7c7dcbbaf8b5aa070
makao <2016333261@qq.com>
2016-10-29 18:47:08 +0800

    mb message jump

--- src/main/java/com/makao/utils/MakaoConstants.java
+++ src/main/java/com/makao/utils/MakaoConstants.java
@@ -10,7 +10,7 @@ public class MakaoConstants {
 	public static final long TOKEN_EXPIRE = 5;//系统自定义token在缓存中保留的时间(暂设为小时)
 	public static final String SERVER_DOMAIN = "http://m1.shequvip.com";//最后部署上线的服务器的域名，用于设置微信回调页面
 	public static final String CDN_DOMAIN="http://sx-1252349799.cosgz.myqcloud.com";//
-	public static final String ENTRANCE = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxdfbe4a14b5c766b5&redirect_uri=http%3a%2f%2fm1.shequvip.com%2fuser%2fsnsapi_userinfo&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect";
+	public static final String ENTRANCE = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxdfbe4a14b5c766b5&redirect_uri=http%3a%2f%2fm1.shequvip.com%2fuser%2fsnsapi_userinfo&response_type=code&scope=snsapi_userinfo&state=2#wechat_redirect";
 	
 	public static final long ORDER_EXPIRE = 10;//下单等待支付的时间限制，单位为分(10)
 	public static final String DEFAULT_CITY_NAME="常州";
