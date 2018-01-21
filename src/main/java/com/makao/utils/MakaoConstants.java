75bf7e5dd37bb63b787bf2379d96432e0d90f5e4
madarou <marco@class100.com>
2018-01-21 20:26:59 +0800

    fix: 修改了公众号

--- src/main/java/com/makao/utils/MakaoConstants.java
+++ src/main/java/com/makao/utils/MakaoConstants.java
@@ -8,7 +8,7 @@ public class MakaoConstants {
 	public static final boolean DEBUG = false;
 	public static final int PAGE_SIZE = 10;//商品列表，券列表，用户列表等分页每页的记录数
 	public static final long TOKEN_EXPIRE = 5;//系统自定义token在缓存中保留的时间(暂设为小时)
-	public static final String SERVER_DOMAIN = "http://demo.shygxx.cn";//最后部署上线的服务器的域名，用于设置微信回调页面
+	public static final String SERVER_DOMAIN = "http://mall.shygxx.cn";//最后部署上线的服务器的域名，用于设置微信回调页面
 	public static final String CDN_DOMAIN="http://sx-1252349799.cosgz.myqcloud.com";//
 	public static final String ENTRANCE = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxc4c94e97f23f699f&redirect_uri=http%3a%2f%2fdemo.shygxx.cn%2fuser%2fsnsapi_userinfo&response_type=code&scope=snsapi_userinfo&state=2#wechat_redirect";
 	
